2442ce0eea94ff62d556dd041c82729849b8d37f
unknown <c00224660@itcarlow.ie>
2020-10-11 18:45:01 +0100

    negative test added

--- RWM-P0-C00224660-Combined/Assets/Tests/CombinedFilterTest.cs
+++ RWM-P0-C00224660-Combined/Assets/Tests/CombinedFilterTest.cs
@@ -19,5 +19,15 @@ namespace Tests
             CollectionAssert.AreEqual(expected, output);
         }
 
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CombinedFilterNegativePasses()
+        {
+            int[] input = { -1, -3, -5, -7, -9 };
+            int[] output = CombinedFilter.combinedFilter(input);
+            int[] expected = { -22, -23, -17, -19, -29 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
     }
 }
