221edd8d6381ee4fa8145dc2cbff9955dbb76bc0
unknown <c00224660@itcarlow.ie>
2020-10-11 18:40:13 +0100

    CombinedFilter done

---
+++ RWM-P0-C00224660-Combined/Assets/Scripts/CombinedFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CombinedFilter : MonoBehaviour
+{
+    public static int[] combinedFilter(int[] xs)
+    {
+        return XOR21Filter.xor21s(DecrementIfLessThanFirstFilter.decrementIfLessThanFirst(xs));
+    }
+
+}
---
+++ RWM-P0-C00224660-Combined/Assets/Tests/CombinedFilterTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CombinedFilterTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CombinedFilterSimplePasses()
+        {
+            int[] input = { 1, 3, 5, 7, 9 };
+            int[] output = CombinedFilter.combinedFilter(input);
+            int[] expected = { 20, 22, 16, 18, 28 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+    }
+}
